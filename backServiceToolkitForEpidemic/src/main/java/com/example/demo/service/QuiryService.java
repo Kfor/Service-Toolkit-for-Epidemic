719a3eee14eaaac6e713ef14de41773467cc5ecc
Kfor <kfor1day@zju.edu.cn>
2020-05-04 00:39:27 +0800

    Fix charset bug.

--- backServiceToolkitForEpidemic/src/main/java/com/example/demo/service/QuiryService.java
+++ backServiceToolkitForEpidemic/src/main/java/com/example/demo/service/QuiryService.java
@@ -8,20 +8,24 @@ import java.net.URLEncoder;
 
 @Service
 public class QuiryService {
-    private final String HOSPITAL = "http://api.map.baidu.com/place/v2/search?query=" + URLEncoder.encode("医院", "UTF-8");
+    private final String HOSPITAL = "http://api.map.baidu.com/place/v2/search?query=医院"
+//            + URLEncoder.encode("医院", "UTF-8")
+            ;
     private final String akkey = "M0998imE0sDc6RvHDIk8UlA8195rw6u1";
 
     public QuiryService() throws UnsupportedEncodingException {
     }
 
-    public String getHospitalPosition(String area) throws Exception{
-        String url = HOSPITAL + "&region=" + URLEncoder.encode(area, "UTF-8") +
+    public String getHospitalPosition(String area) throws Exception {
+        String url = HOSPITAL + "&region=" + area +
+//                + URLEncoder.encode(area, "UTF-8") +
                 "&output=json&ak=" + akkey;
 
         RestTemplate restTemplate = new RestTemplate();
         String result = restTemplate.getForObject(url, String.class);
         System.out.println(url);
         System.out.println(result);
+
         return result;
     }
 
